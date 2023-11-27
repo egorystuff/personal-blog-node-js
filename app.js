26e00298c4588468275358bfd69080c1b021b5a4
Yahor Yankavets <mr.yankovec@gmail.com>
2023-11-27 16:49:15 +0300

    new commit

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.readFile("./test.txt", "utf-8", (error, data) => {
-  fs.mkdirSync("./files", () => {});
+// fs.readFile("./test.txt", "utf-8", (error, data) => {
+//   fs.mkdirSync("./files", () => {});
 
-  fs.writeFileSync("./files/test2.txt", `${data}New text!`, (error) => {
-    error ? console.log(error) : null;
-  });
+//   fs.writeFileSync("./files/test2.txt", `${data}New text!`, (error) => {
+//     error ? console.log(error) : null;
+//   });
+// });
+
+// setTimeout(() => {
+//   if (fs.existsSync("./files/test2.txt")) {
+//     fs.unlink("./files/test2.txt", () => {});
+//   }
+// }, 4000);
+// setTimeout(() => {
+//   if (fs.existsSync("./files")) {
+//     fs.rmdir("./files", () => {});
+//   }
+// }, 6000);
+
+const Logger = require("./log");
+const logger = new Logger();
+
+logger.on("some_event", (args) => {
+  const { id, text } = args;
+  console.log(id, text);
 });
 
-setTimeout(() => {
-  if (fs.existsSync("./files/test2.txt")) {
-    fs.unlink("./files/test2.txt", () => {});
-  }
-}, 4000);
-setTimeout(() => {
-  if (fs.existsSync("./files")) {
-    fs.rmdir("./files", () => {});
-  }
-}, 6000);
-
-//console.log("Just test!");
+logger.log("User Logged");
