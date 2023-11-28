84cff8bb2f29fa8e555731b9c4e7e370d410d633
Yahor Yankavets <mr.yankovec@gmail.com>
2023-11-28 16:57:44 +0300

    added express for server

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log("Server start");
-  console.log(req.url, req.method);
+  console.log("Nodemon start");
 
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text/html");
 
-  const data = JSON.stringify([
-    { name: "Tommy", age: 35 },
-    { name: "Arthur", age: 40 },
-  ]);
+  const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
 
-  // res.write('<head><link rel="stylesheet" href="#"></head>');
-  // res.write("<h1>Hello world!</h1>");
-  // res.write("<p>My name is Yahor</p>");
-  res.end(data);
+  let basePath = "";
+
+  switch (req.url) {
+    case "/":
+    case "/home":
+    case "/index.html":
+      basePath = createPath("index");
+      res.statusCode = 200;
+      break;
+
+    case "/about-us":
+      res.statusCode = 301;
+      res.setHeader("Location", "/contacts");
+      res.end();
+
+    case "/contacts":
+      basePath = createPath("contacts");
+      res.statusCode = 200;
+      break;
+
+    default:
+      basePath = createPath("error");
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(basePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(PORT, "localhost", (error) => {
