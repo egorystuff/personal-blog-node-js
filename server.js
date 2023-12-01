79b18190ad4cb5b2378f731291f82b8d968b4f6e
Yahor Yankavets <mr.yankovec@gmail.com>
2023-12-01 14:17:16 +0300

    updated something

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require("express");
+const chalk = require("chalk");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const methodOverride = require("method-override");
 const postRouter = require("./routes/post-routes");
 const postApiRouter = require("./routes/api-post-routes");
 const contactsRouter = require("./routes/contacts-routes");
 const createPath = require("./helpers/create-path");
 
+const errorMsg = chalk.bgKeyword("white").redBright;
+const successMsg = chalk.bgKeyword("green").white;
+
 const app = express();
 
 app.set("view engine", "ejs");
 
-const PORT = 3000;
-const db = "mongodb+srv://Yahor:lantirn1994@cluster0.chix9ce.mongodb.net/node-blog?retryWrites=true&w=majority";
-
 mongoose
-  .connect(db)
-  .then((res) => console.log("Connect to DB"))
-  .catch((error) => console.log(error));
+  .connect(process.env.MONGO_URL)
+  .then((res) => console.log(successMsg("Connect to DB")))
+  .catch((error) => console.log(errorMsg(error)));
 
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
+app.listen(process.env.PORT, (error) => {
+  error ? console.log(errorMsg(error)) : console.log(successMsg(`listening port ${process.env.PORT}`));
 });
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
